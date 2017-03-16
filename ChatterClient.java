ae02e07fdfd420d25e7af3ad8216fa3d721ce913
lwathieu1 <lbw29@georgetown.edu>
2017-03-15 22:42:17 -0400

    finished Chatter project

--- ChatterClient.java
+++ ChatterClient.java
@@ -1,16 +1,22 @@
 package lbw29;
 
+//Joint Project by:
+//    Lancelot Wathieu
+//    Jack Gerli
+
 import java.io.*;
 import java.net.Socket;
 import java.util.*;
 
+//Client side of a Chat room. Each client should run this file.
 public class ChatterClient
 {
 	private ClientListener listener;
 	private Scanner scanner;
 	private BufferedWriter bout;
-	Socket socket;
+	private Socket socket;
 
+	//Starts a Chatter client with inputted hostname and portnumber, or with defaults 127.0.0.1 and 11029
 	public static void main( String[] args )
 	{	
 		if (args.length == 2) 
@@ -20,40 +26,50 @@ public class ChatterClient
 		else
 		{
 			System.out.println("Use format: java ChatterClient <host name> <port number>");
-            System.out.println("Using default hostname \"127.0.0.1\" and port 11123 ");
-            new ChatterClient("127.0.0.1", 11123);
+            System.out.println("Using default hostname \"127.0.0.1\" and port 11029 ");
+            new ChatterClient("127.0.0.1", 11029);
 		}
 	}
 
-	
+	//constructor for ChatterClient Class
 	public ChatterClient(String name, int port)
 	{		
 		try
 		{
+			//this command will throw an exception if "port" is not open
 	        socket = new Socket(name, port);
-	         
+	        
 	        OutputStream out = socket.getOutputStream();
 		    bout = new BufferedWriter( new OutputStreamWriter( out ) );
 		    
 	 		scanner = new Scanner(System.in);
-			System.out.println("Welcome! What is your nickname (no spaces)?");
+			System.out.println("Welcome to the Chat Room! What is your nickname (no spaces)?");
 			String nickName = getUserInput();
 			write("/nick " + nickName);
+			System.out.println("Welcome " + nickName + "!");
+			System.out.println("Simply type for group messages, or use these commands:");
+			System.out.println("           /nick NAME (change nickname)");
+			System.out.println("           /dm NAME MSG (Send personal MSG to NAME)");
+			System.out.println("           /users (lists all current users of the chat room)");
+			System.out.println("           /quit (Leave the chat room)\n");
+			
+			
 		     
 	        listener = new ClientListener(socket);
 	        listener.start();
 	         
+	        //The main thread stays here and checks for keyboard commands (and sends them to the server) continuously 
+	        //until the user types /quit
 	        while(!socket.isClosed())
 	        {
 	        	String userCommand = getUserInput();
 	        	write(userCommand);
 	        	if (userCommand.split(" ")[0].equals("/quit"))
 	        	{
-	        		System.out.println("Thanks for using this!");
+	        		System.out.println("Thanks for using the Chat Room!");
 	        		socket.close();
 	        	}
 	        }
-	        System.out.println("Out of while loop!");
 	        System.exit(0);
 				
 	    }
@@ -61,37 +77,40 @@ public class ChatterClient
 	     { System.err.println(e); }
 	}
 	
-	
+	//Returns keyboard input, blocks until enter is hit
 	private String getUserInput()
 	{
 		String line = scanner.nextLine();
 		return line;
 	}
 	
+	//writes to the server using a BufferedWriter, adding a "\n" so that the server can use "readline()"
 	public void write(String s) throws IOException 
 	{
         bout.write(s + "\n");
         bout.flush();
 	}
 	
+	//Thread that continuously listens on the socket for ChatterServer messages
 	public class ClientListener extends Thread
 	{
 		private BufferedReader bin;
 		
-		
+		//constructor, sets up the BufferedReader "bin
 		public ClientListener(Socket s) throws Exception
 		{
-
 	        InputStream in = s.getInputStream();
 	        bin = new BufferedReader( new InputStreamReader(in) );
 		}
 		
+		//Blocks until the server flushes a message to the socket
 		public String readLine() throws IOException
 		{
 	         String line = bin.readLine();
 	         return line;
 		}
 		
+		//runs continuously
 		@Override
 	    public void run()
 	    {
@@ -99,13 +118,14 @@ public class ChatterClient
 			{
 				while(true)
 				{
+					//prints the server's message as long as it's not empty
 					String serverLine = readLine();
-					System.out.println(serverLine);
+					if (!serverLine.isEmpty())
+						System.out.println(serverLine);
 				}
 			}
 			catch (Exception e) 
 			{e.printStackTrace();}
 	    }
-		
 	}
 }
